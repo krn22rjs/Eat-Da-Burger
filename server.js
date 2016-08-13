c969e6093be2112811b2cac8eddcf614dad30a5c
Karina Rojas <Karina Rojas>
2016-08-13 19:40:02 -0400

    still working on it

---
+++ server.js
@@ -0,0 +1,11 @@
+/*
+Here is where you set up your server file.
+express middleware.
+*/
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var 
+
+var app = express();
