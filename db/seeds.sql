0bdd362b6a0148aeed21363ee1b37513b3f87fbb
Karina Rojas <Karina Rojas>
2016-08-13 20:01:30 -0400

    discovered that timestamp was automatic

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers(burger_name, devoured,date) VALUES ("garden burger", FALSE);
-INSERT INTO burgers(burger_name, devoured,date) VALUES ("turkey burger", FALSE);
-INSERT INTO burgers(burger_name, devoured,data) VALUES ("triple cheeseburger", FALSE)
+INSERT INTO burgers(burger_name, devoured) VALUES ("garden burger", FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES ("turkey burger", FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES ("triple cheeseburger", FALSE);
