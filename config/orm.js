438d8a2ba3f8ae673dd5396d9ad2c82573f356eb
Karina Rojas <Karina Rojas>
2016-08-13 20:26:34 -0400

    burger.js file

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require('./connection.js');
+
+var orm = {
+    selectByBurger: function () {
+        var burgersToEat = 'SELECT burger_name FROM burgers ORDER BY date ;';
+        connection.query(s, function (err, result) {
+
+            console.log(result);
+        });
+    },
+    findByName: function (burger_name) {
+        var queryString = 'SELECT * FROM burgers WHERE burger_name = ?';
+        connection.query(queryString, [burger_name], function (err, result) {
+
+            console.log(result);
+        });
+    },
+    devouredBurgers: function () {
+        var eaten = 'SELECT * FROM burgers WHERE devoured = TRUE';
+
+        connection.query(s, function (err, result) {
+            console.log(result);
+        });
+    }
+};
+
+module.exports = orm;
